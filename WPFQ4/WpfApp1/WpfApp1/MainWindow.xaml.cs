03d8457845f2cdfa63f9315f6da9a693274a0c95
Keanu191 <30074191@tafe.wa.edu.au>
2024-11-04 21:20:02 +0800

    Started project
    Started the project and I have the following problems:
    
    -CsvHelper.HeaderValidationException: 'Header with name 'TKey'[0] was not found.,

---
+++ WPFQ4/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CsvHelper;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            readCSV(); // call ReadCSV file when the GUI loads per 4.2 assessment requirements
+        }
+
+        // 4.1 Create a Dictionary data structure with a TKey of type integer and a TValue of type string, name the new data structure “MasterFile”. 
+        Dictionary<int, string> MasterFile = new Dictionary<int, string>();
+
+
+        // 4.2 Create a method that will read the data from the .csv file into the Dictionary data structure when the GUI loads.          
+        private void readCSV()
+        {
+            // probably won't work, would need to be more direct with the file location but thats for another day
+            using (var reader = new StreamReader(@"C:\\Users\\noawa\\Documents\\GitHub\\MalinAT3\\WPFQ4\\WpfApp1\\WpfApp1\\MalinStaffNamesV3.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                // read records from the CSV file and populate the dictionary
+                var records = csv.GetRecords<Record>();
+
+                foreach (var record in records)
+                {
+                    MasterFile[record.TKey] = record.TValue; // add to dictionary
+                }
+            }
+        }
+    }
+}
---
+++ WPFQ4/WpfApp1/WpfApp1/Record.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Record
+    {
+        // getters and setters
+        public int TKey {  get; set; }
+        public string TValue { get; set; }
+    }
+}
