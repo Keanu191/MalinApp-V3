6f7510963f6c67dc24efcd608edccfc7afa3efdd
Keanu191 <30074191@tafe.wa.edu.au>
2024-11-05 15:01:06 +0800

    Up to 4.3
    - Done the streamreader for 4.2 thanks to jack
    
    - up to 4.3 now

--- WPFQ4/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WPFQ4/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            readCSV(); // call ReadCSV file when the GUI loads per 4.2 assessment requirements
+            // call ReadCSV file when the GUI loads per 4.2 assessment requirements, set missingFieldFound to null as what the exception suggested
+            readCSV();
         }
 
         // 4.1 Create a Dictionary data structure with a TKey of type integer and a TValue of type string, name the new data structure “MasterFile”. 
@@ -33,19 +34,58 @@ namespace WpfApp1
 
         // 4.2 Create a method that will read the data from the .csv file into the Dictionary data structure when the GUI loads.          
         private void readCSV()
-        {
-            // probably won't work, would need to be more direct with the file location but thats for another day
-            using (var reader = new StreamReader(@"C:\\Users\\noawa\\Documents\\GitHub\\MalinAT3\\WPFQ4\\WpfApp1\\WpfApp1\\MalinStaffNamesV3.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        { 
+            // create stream reader
+            StreamReader? reader = null;
+            if (File.Exists("MalinStaffNamesV3.csv"))
             {
-                // read records from the CSV file and populate the dictionary
-                var records = csv.GetRecords<Record>();
-
-                foreach (var record in records)
+                // read provided csv file
+                reader = new StreamReader(File.OpenRead("MalinStaffNamesV3.csv"));
+                while (!reader.EndOfStream)
                 {
-                    MasterFile[record.TKey] = record.TValue; // add to dictionary
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    // if there isn't a comma then add values
+                    if (line != ",")
+                    {
+                        string s = values[0];
+                        string t = values[1];
+                        // due to the dictionary being formatted of int, string, we will have to parse S to represent the integer
+                        MasterFile.Add(int.Parse(s), t);
+
+                        // display success message
+                        statusBarText.Text = "The CSV file has been loaded successfully!";
+                    }
                 }
             }
+            else
+            {
+                // if CSV file wasn't found then display error message
+                statusBarText.Text = "ERROR: The CSV file could not be found!";
+            }
+        }
+
+        /*
+         * 4.3 Create a method to display the Dictionary data into a non-selectable display only list box (ie read only). 
+         */
+        private void displayDictionary()
+        {
+            // clear items in the listbox
+            ListBox1.Items.Clear();
+
+            ListBox1.Items.Add();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            displayDictionary(); // call displayDictionary method
         }
     }
 }
